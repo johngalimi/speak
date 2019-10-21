f5c937c9576493d8ee9a60a7090c4865f21db763
johngalimi <galimijohn@gmail.com>
2019-10-21 16:39:27 -0400

    serving html

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 const express = require('express');
-const app = express();
+
+var app = require('express')();
+
 const http = require('http').Server(app)
 const socket_io = require('socket.io')(http);
 
 app.get('/', function(request, response) {
-	response.send('hello world');
+	//response.send('hello world');
+	response.sendFile(__dirname + '/index.html');
 });
 
-const server = http.listen(8080, function() {
-	console.log('listening on *:8080');
+/*
+socket_io.on('connection', function(socket) {
+	console.log('user connected');
+	socket.on('chat message', function(message) {
+		socket_io.emit('chat message', message);
+	});
+	socket.on('disconnect', function() {
+		console.log('user disconnected');
+	});
 });
+*/
+
+socket_io.on('connection', function(socket) {
+	console.log('user connected');
+});
+
+const server = http.listen(3000, function() {
+	console.log('listening on *:3000');
+})
+
